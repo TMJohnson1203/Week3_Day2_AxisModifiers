265094f4431d914203aa005724334be77fa7dc03
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-10 11:49:22 -0400

    Halfday commit

--- Week3_Day2_AxisModifiers/Program.cs
+++ Week3_Day2_AxisModifiers/Program.cs
@@ -8,8 +8,14 @@ namespace Week3_Day2_AxisModifiers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // create variable "boat," name it and then give it a value "new"
         {
+            Boat boaty = new Boat(3, "Boaty", 18.6);   //what constructor do we want to use?
+            boaty.Move();
+            boaty.BoatInfo();
+
+            Boat boat2 = new Boat(14, "Boat 2", 30.8);
+            boat2.BoatInfo();
         }
     }
 }
---
+++ Week3_Day2_AxisModifiers/boat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day2_AxisModifiers
+{
+    class Boat
+    {
+        public int numberOfPassengers;
+        private string name;
+        public  double engineSize;
+
+        public string Name
+        {
+            get { return name; } // could be {return this.name}
+            set { name = value; } // could be {this.name = value}
+        }
+
+        public Boat()// contructor = constructs an instance =default
+        {
+
+        }
+        public Boat(int numberOfPassengers, string name, double engineSize)
+        {
+            this.numberOfPassengers = numberOfPassengers;
+            this.name = name;
+            this.engineSize = engineSize;
+        }
+
+        // BELOW are methods
+
+        public void Move() //empty, so it is not going to return anything back 
+        {
+            Console.WriteLine(name + " moves forward."); // method of movement
+        }
+
+        
+        private double CalculateMPG()// //METHOD  can only be accessed in the BOAT class=only stuff in the class "knows" about what is inside {}
+        {
+            double MPG = engineSize * .7d;
+            return MPG;
+        }
+
+        public void BoatInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Passengers: " + numberOfPassengers);
+            Console.WriteLine("Engine size: " + engineSize);
+            Console.WriteLine("MPG " + CalculateMPG());
+            Console.WriteLine();
+        }
+    }
+}
